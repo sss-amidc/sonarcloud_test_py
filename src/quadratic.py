219497cb00fa1e3e4ea54480b6432c0063beec73
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-08-14 16:43:55 +0530

    Deliberately introduced some bugs

--- src/quadratic.py
+++ src/quadratic.py
@@ -13,12 +13,12 @@ b = 5
 c = 6
 
 # Calculate the discriminant
-d = (b**2) - (4 * a * c)
+d = (b * * 2) - (4 * a * c)
 
 # Find solutions
 sol1 = (-b - cmath.sqrt(d)) / (2 * a)
 sol2 = (-b + cmath.sqrt(d)) / (2 * a)
 
 # Finally, print the result
-print(f"sol1: {sol1}, sol2: {sol2}")
+print(f"sol1: {sol1}, sol2: {sol2}"
 
