2059edcbc0ba57aeec9605555fdddf1014674127
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-09-06 15:22:01 +0530

    Introduced a bug

--- src/quadratic.py
+++ src/quadratic.py
@@ -16,7 +16,7 @@ c = 6
 d = (b ** 2) - (4 * a * c)
 
 # Find solutions
-sol1 = (-b - cmath.sqrt(d)) / (2 * a)
+sol1 = (-b - cmath.sqrt(d) / (2 * a)
 sol2 = (-b + cmath.sqrt(d)) / (2 * a)
 
 # Finally, print the result
