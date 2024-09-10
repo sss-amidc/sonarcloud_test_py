7b3a0260c3af23d6e4210c100dd6ec52dfc9bf2f
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-09-10 11:45:07 +0530

    A simple test script for demo purposes has been added

---
+++ src/test_demo_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+    Simple program to find the solution to a quadratic
+    equation using python3
+    Also, added some pytest unit-test functions
+'''
+
+import math
+
+
+def test_sqrt():
+    num = 25
+    assert math.sqrt(num) == 5
+
+
+def testsquare():
+    num = 7
+    assert (7*7) == 40
+
