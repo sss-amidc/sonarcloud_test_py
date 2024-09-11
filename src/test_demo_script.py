56c0c9cb01d1c3a04c540cbcbb541ff3b48d2799
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-09-11 16:49:24 +0530

    For demo purposes - 6

--- src/test_demo_script.py
+++ src/test_demo_script.py
@@ -6,15 +6,17 @@
     Also, added some pytest unit-test functions
 '''
 
-import math
+# import math
 
 
 def test_sqrt():
+    ''' Dummy function 1 '''
     # num = 25
     # assert math.sqrt(num) == 5
     pass
 
 def testsquare():
+    ''' Dummy function 2 '''
     #num = 7
     # assert (7*7) != 40
     pass
