e94522e70e6dbbb5d076b83283d9bd85fb7de8a7
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-09-11 16:50:27 +0530

    For demo purposes - 7

--- src/test_demo_script.py
+++ src/test_demo_script.py
@@ -13,10 +13,8 @@ def test_sqrt():
     ''' Dummy function 1 '''
     # num = 25
     # assert math.sqrt(num) == 5
-    pass
 
 def testsquare():
     ''' Dummy function 2 '''
     #num = 7
     # assert (7*7) != 40
-    pass
