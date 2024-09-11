9efebe49004cd65a692dfdc9681d1b531b9a5ad6
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2024-09-11 16:44:38 +0530

    For demo purposes - 4

--- src/test_demo_script.py
+++ src/test_demo_script.py
@@ -10,11 +10,11 @@ import math
 
 
 def test_sqrt():
-    num = 25
-    assert math.sqrt(num) == 5
+    # num = 25
+    # assert math.sqrt(num) == 5
 
 
 def testsquare():
-    num = 7
-    assert (7*7) != 40
+    #num = 7
+    # assert (7*7) != 40
 
